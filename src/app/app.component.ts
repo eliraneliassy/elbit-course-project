5efc3b484434e43b506bb6c05c0a9f266c01ccf5
Eliran Eliassy <eliran.eliassy@gmail.com>
2023-01-05 10:40:10 +0200

    book component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,22 @@ export class AppComponent {
     price: 90.00,
     previewImgUrl: `https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/5903/9780590353427.jpg`
   };
+
+  book2: Book = {
+    title: 'The Lord of the Rings',
+    price: 140.00,
+    previewImgUrl: `https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/5440/9780544003415.jpg`
+  };
+
+  book3: Book = {
+    title: 'Angular Projects : Build modern web apps by exploring Angular 12 with 10 different projects and cutting-edge technologies, 2nd Edition',
+    price: 240.00,
+    previewImgUrl: `https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/8002/9781800205260.jpg`
+  };
+
+  books: Book[] = [
+    this.book1, this.book2, this.book3
+  ]
+  
+
 }
---
+++ src/app/book/book.component.ts
@@ -0,0 +1,11 @@
+import { Book } from './../book.interface';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss']
+})
+export class BookComponent {
+  @Input() book?: Book;
+}
