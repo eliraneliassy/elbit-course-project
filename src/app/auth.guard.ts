0723c28cfacae89e2333cf21c81d756b01cfe216
Eliran Eliassy <eliran.eliassy@gmail.com>
2023-01-12 15:49:10 +0200

    fix guard bug

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,6 +1,6 @@
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,12 +8,19 @@ import { Observable } from 'rxjs';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot):
     Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.isLoggedIn();
+    const result = this.authService.isLoggedIn();
+    if (!result) {
+      this.router.navigateByUrl('/login');
+    }
+
+    return true;
+
+
   }
 
 }
