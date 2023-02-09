fe5257b357fdd57b4437d9a6197fcb0fa72d2bca
Eliran Eliassy <eliran.eliassy@gmail.com>
2023-02-09 11:34:33 +0200

    change detection

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss'],
-  // changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 
 })
 export class BookComponent {
